e5a08bed8f5f4749a81267ce61cf8cef0a9b4edd
Garrett Huon <ghuon407@gmail.com>
2021-09-15 22:02:32 -0400

    menu start

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -11,10 +11,33 @@ namespace UI
             do{
                Console.WriteLine("Welcome to the Shoppe");
                Console.WriteLine("What would you like to do?");
-               Console.WriteLine("[1]");
-               Console.WriteLine("[2]");
+               Console.WriteLine("[1]See Brands");
+               Console.WriteLine("[2]See Cart");
+               Console.WriteLine("[3]History");
+               Console.WriteLine("[x]Leave");
 
                input = Console.ReadLine();
+
+               switch (input)
+               {
+                   case "1" :
+                   Console.WriteLine("Here are your options:");
+                   break;
+
+                   case "2" :
+                   Console.WriteLine("In your cart");
+                   break;
+
+                   case "3" :
+
+                   break;
+
+                   case "x" :
+                   Console.WriteLine("Bye Bye");
+                   exit = true;
+                   break;
+               }
+
 
             } while (!exit);
 
